edd3b3f82d3b90bf164356f8c233ebfc8a9bd4c6
devalexer <alliemckay.dev@gmail.com>
2017-04-01 23:04:44 -0400

    Basic button clicks and calculations functional, some errors

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -25,53 +25,85 @@ namespace Calculator
             InitializeComponent();
             this.Title = "Standard Calculator";
         }
-     
-        private void FirstUserInput(object sender, TextChangedEventArgs e)
-        {
-            //pulls users 1st input from first box
-            var input1 = this.Input1.Text;
-        }
 
-        private void SecondUserInput(object sender, TextChangedEventArgs e)
-        {
-            //pulls users 2nd number input from second box
-            var input2 = this.Input2.Text;
-        }
+        string opr;
+        double a, b, result;
+
+        //private void FirstUserInput(object sender, TextChangedEventArgs e)
+        //{
+        //    //pulls users 1st input from first box
+        //    var input1 = this.Input1.Text;
+        //}
+
+        //private void SecondUserInput(object sender, TextChangedEventArgs e)
+        //{
+        //    //pulls users 2nd number input from second box
+        //    var input2 = this.Input2.Text;
+        //}
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            //performs addition function
-            var button = (Button)sender;
-            int total = Int32.Parse(Input1.Text) + Int32.Parse(Input2.Text);
-            this.Calculation.Text = total.ToString();
+            //takes 1st input, converts to double, saves in "a", attaches operator, clears window
+            if (Calculation.Text == "")
+                Calculation.Text = "";
+            else
+            {
+                a = double.Parse(Calculation.Text);
+                opr = "+";
+                Calculation.Text = String.Empty;
+            }
         }
 
         private void Subtract_Click(object sender, RoutedEventArgs e)
         {
-            //performs subtraction function
-            int total = Int32.Parse(Input1.Text) - Int32.Parse(Input2.Text);
-            this.Calculation.Text = total.ToString();
+            //takes 1st input, converts to double, saves in "a", attaches operator, clears window
+            if (Calculation.Text == "")
+                Calculation.Text = "";
+            else
+            {
+                a = double.Parse(Calculation.Text);
+                opr = "-";
+                Calculation.Text = String.Empty;
+            }
         }
 
         private void Mult_Click(object sender, RoutedEventArgs e)
         {
-            //performs multiplication function
-            int total = Int32.Parse(Input1.Text) * Int32.Parse(Input2.Text);
-            this.Calculation.Text = total.ToString();
+            if (Calculation.Text == "")
+                Calculation.Text = "";
+            //takes 1st input, converts to double, saves in "a", attaches operator, clears window
+            else
+            {
+                a = double.Parse(Calculation.Text);
+                opr = "*";
+                Calculation.Text = String.Empty;
+            }
         }
 
         private void Div_Click(object sender, RoutedEventArgs e)
         {
-            //performs division function
-            int total = Int32.Parse(Input1.Text) / Int32.Parse(Input2.Text);
-            this.Calculation.Text = total.ToString();
+            if (Calculation.Text == "")
+                Calculation.Text = "";
+            //takes 1st input, converts to double, saves in "a", attaches operator, clears window
+            else
+            {
+                a = double.Parse(Calculation.Text);
+                opr = "/";
+                Calculation.Text = String.Empty;
+            }
         }
 
         private void Percent_Click(object sender, RoutedEventArgs e)
         {
-            //performs percentage function
-            int total = (Int32.Parse(Input1.Text) / Int32.Parse(Input2.Text)) * 100;
-            this.Calculation.Text = total.ToString();
+            if (Calculation.Text == "")
+                Calculation.Text = "";
+            //takes 1st input, converts to double, saves in "a", attaches operator, clears window
+            else
+            {
+                a = double.Parse(Calculation.Text);
+                opr = "%";
+                Calculation.Text = String.Empty;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -80,17 +112,76 @@ namespace Calculator
             if (Calculation.Text == "0")
                 Calculation.Text = string.Empty;
             
-            //for any button with Button_Click event, prints number of key to Calculation box
+            //for any button with Button_Click event, prints number of that key to Calculation box
             Button button = (Button)sender;
-            Calculation.Text = Calculation.Text + button.Content;
+            if ((button == null) || (button.ContentStringFormat == "0"))
+                return;
+
+            Calculation.Text += button.Content;
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            if (Calculation.Text == "")
+                Calculation.Text = "";
+            else
+            {
+                b = Convert.ToDouble(Calculation.Text);
+
+                switch (opr)
+                {
+                    case "+":
+                        result = a + b;
+                        Calculation.Text = result.ToString();
+                        break;
+
+                    case "-":
+                        result = a - b;
+                        Calculation.Text = result.ToString();
+                        break;
+
+                    case "*":
+                        result = a * b;
+                        Calculation.Text = result.ToString();
+                        break;
+
+                    case "/":
+                        result = a / b;
+                        Calculation.Text = result.ToString();
+                        break;
+
+                    case "%":
+                        result = a % b;
+                        Calculation.Text = result.ToString();
+                        break;
+
+                    case "x^2":
+                        result = Math.Pow(a, 2);
+                        Calculation.Text = result.ToString();
+                        break;
+                }
+            }
         }
 
         private void CE_Click(object sender, RoutedEventArgs e)
         {
+            //CE button clears current text in box
+            Calculation.Text = "";
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
             //CE button clears all and resets calculator
             this.Calculation.Text = "";
-            this.Input1.Text = string.Empty;
-            this.Input2.Text = string.Empty;
+        }
+
+        private void PosNeg_Click(object sender, RoutedEventArgs e)
+        {
+            //toggles positive or negative quality 
+            if (Calculation.Text.Contains('-'))
+                Calculation.Text.Replace("-", "");
+            else
+                Calculation.Text = "-" + Calculation.Text;
         }
     }
 }
